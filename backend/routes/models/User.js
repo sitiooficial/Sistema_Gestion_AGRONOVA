620bbe2ae946022b17c83fb7b5a279f00f6b83e7
sitiooficial <104956506+sitiooficial@users.noreply.github.com>
2025-11-27 10:53:37 -0500

    Create User.js

---
+++ backend/routes/models/User.js
@@ -0,0 +1,10 @@
+///models/User.js
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  return sequelize.define("User", {
+    username: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false }
+  });
+};
